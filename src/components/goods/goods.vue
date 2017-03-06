2d20a87b4b1e34fad5b9ec049adda3533a8023fe
Charlie <Charlie@Charlie-RMBP.local>
2017-03-06 22:28:21 +0800

    goods details 20170306

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -15,7 +15,7 @@
     		<li v-for="item in goods" class="food-list food-list-hook">
     			<h1 class="title">{{item.name}}</h1>
     			<ul>
-    				<li v-for="food in item.foods" class="food-item">
+    				<li @click="selectFood(food, $event)" v-for="food in item.foods" class="food-item">
     					<div class="icon">
     						<img :src="food.icon" width="57" height="57" alt="">
     					</div>
@@ -40,6 +40,7 @@
     	</ul>
     </div>
     <shopcart v-ref:shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <food :food="selectedFood" v-ref:food></food>
   </div>
 </template>
 
@@ -47,6 +48,7 @@
 	import BScroll from 'better-scroll'
 	import shopcart from 'components/shopcart/shopcart'
 	import cartcontrol from 'components/cartcontrol/cartcontrol'
+	import food from 'components/food/food'
 
 	const ERR_OK = 0
 
@@ -60,7 +62,8 @@
 			return {
 				goods: [],
 				listHeight: [],
-				scrollY: 0
+				scrollY: 0,
+				selectedFood: {}
 			}
 		},
 		computed: {
@@ -108,6 +111,13 @@
 				let el = foodList[index]
 				this.foodsScroll.scrollToElement(el, 300)
 			},
+			selectFood(food, event) {
+				if (!event._constructed) {
+					return
+				}
+				this.selectedFood = food
+				this.$refs.food.show()
+			},
 			_drop(target) {
 				// 优化体验，异步执行下落动画
 				this.$nextTick(() => {
@@ -139,7 +149,8 @@
 		},
 		components: {
 			shopcart,
-			cartcontrol
+			cartcontrol,
+			food
 		},
 		events: {
 			'cart.add'(target) {
@@ -261,51 +272,3 @@
 						right: 0
 						bottom: 6px
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

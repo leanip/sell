08e503e08d8d4e83ba7748c7e2e30958489d96fe
Charlie <Charlie@Charlie-RMBP.local>
2017-03-23 22:46:15 +0800

    seller info

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -43,6 +43,23 @@
   			</ul>
   		</div>
   		<split></split>
+  		<div class="pics">
+  			<h1 class="title">商家实景</h1>
+  			<div class="pic-wrapper" v-el:pic-wrapper>
+  				<ul class="pic-list" v-el:pic-list>
+  					<li class="pic-item" v-for="pic in seller.pics">
+  						<img :src="pic" width="120" height="90">
+  					</li>
+  				</ul>
+  			</div>
+  		</div>
+  		<split></split>
+  		<div class="info">
+  			<h1 class="title border-1px">商家信息</h1>
+  			<ul>
+  				<li class="info-item" v-for="info in seller.infos">{{info}}</li>
+  			</ul>
+  		</div>
   	</div>
   </div>
 </template>
@@ -64,10 +81,12 @@
 		watch: {
 			'seller'() {
 				this._initScroll()
+				this._initPics()
 			}
 		},
 		ready() {
 			this._initScroll()
+			this._initPics()
 		},
 		methods: {
 			_initScroll() {
@@ -76,6 +95,24 @@
 				} else {
 					this.scroll = new BScroll(this.$els.seller, {
 						click: true
+					})
+				}
+			},
+			_initPics() {
+				if (this.seller.pics) {
+					let picWidth = 120
+					let margin = 6
+					let width = (picWidth + margin) * this.seller.pics.length - margin
+					this.$els.picList.style.width = width + 'px'
+					this.$nextTick(() => {
+						if (this.picScroll) {
+							this.picScroll.refresh()
+						} else {
+							this.picScroll = new BScroll(this.$els.picWrapper, {
+								scrollX: true,
+								eventPassthrough: 'vertical'
+							})
+						}
 					})
 				}
 			}
@@ -181,4 +218,39 @@
 						line-height: 16px
 						font-size: 12px
 						color: rgb(7, 17, 27)
+		.pics
+			padding: 18px
+			.title
+				margin-bottom: 12px
+				line-height: 14px
+				font-size: 14px
+				color: rgb(7, 17, 27)
+			.pic-wrapper
+				width: 100%
+				overflow: hidden
+				white-space: nowrap
+				.pic-list
+					font-size: 0
+					.pic-item
+						display: inline-block
+						margin-right: 6px
+						width: 120px
+						height: 90px
+						&:last-child
+							margin-right: 0
+		.info
+			padding: 18px 18px 0
+			color: rgb(7, 17, 27)
+			.title
+				padding-bottom: 12px
+				line-height: 14px
+				font-size: 14px
+				border-1px(rgba(7, 17, 27, 0.1))
+			.info-item
+				padding: 16px 12px
+				line-height: 16px
+				border-1px(rgba(7, 17, 27, 0.1))
+				font-size: 12px
+				&:last-child
+					border-none()
 </style>

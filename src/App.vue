dd3390f41bfd3a2a35af37f9a904eaf10a678d41
CharlieZhang <charliezhang@zhen-tao.com>
2017-03-24 18:22:59 +0800

    completed

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,12 @@
       <div class="tab-item"><a v-link="{path:'/ratings'}">评论</a></div>
       <div class="tab-item"><a v-link="{path:'/seller'}">商家</a></div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <router-view :seller="seller" keep-alive></router-view>
   </div>
 </template>
 
 <script type="script/ecmascript-6">
+import {urlParse} from 'common/js/util'
 import header from './components/header/header'
 
 const ERR_OK = 0
@@ -18,14 +19,19 @@ const ERR_OK = 0
 export default {
   data() {
     return {
-      seller: {}
+      seller: {
+        id: (() => {
+          let queryParam = urlParse();
+          return queryParam.id;
+        })()
+      }
     }
   },
   created() {
-    this.$http.get('/api/seller').then((res) => {
+    this.$http.get('/api/seller?id='+this.seller.id).then((res) => {
       res = res.body
       if(res.errno === ERR_OK){
-        this.seller = res.data
+        this.seller = Object.assign({}, this.seller, res.data)
       }
     })
   },
